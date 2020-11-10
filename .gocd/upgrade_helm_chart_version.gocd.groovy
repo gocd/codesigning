987701ebb3966cb5c6bcda149c39d5a7f264ab0a
Kritika Singh <kritika.singh1994@gmail.com>
2020-11-10 11:01:46 +0530

    Remove job to send a pr at helm/stable repo (deprecated) Update the PR description for gocd/gocd-helm

--- .gocd/upgrade_helm_chart_version.gocd.groovy
+++ .gocd/upgrade_helm_chart_version.gocd.groovy
@@ -19,41 +19,13 @@ GoCD.script {
         }
       }
       stages {
-        stage('send_pr_to_helm_chart_repo') {
+        stage('send_pr_to_gocd_helm_chart_repo') {
           artifactCleanupProhibited = false
           cleanWorkingDir = false
           fetchMaterials = true
           approval {
           }
           jobs {
-            job('update_at_helm_chart_repo') {
-              elasticProfileId = 'ecs-gocd-dev-build'
-              environmentVariables = [
-                GIT_USERNAME: 'gocd-ci-user',
-                GIT_PASSWORD: '{{SECRET:[build-pipelines][GOCD_CI_USER_RELEASE_TOKEN]}}',
-              ]
-              tasks {
-                fetchArtifact {
-                  destination = 'codesigning'
-                  job = 'dist'
-                  pipeline = 'installers/code-sign/PublishStableRelease'
-                  runIf = 'passed'
-                  source = 'dist/meta/version.json'
-                  stage = 'dist'
-                  file = true
-                }
-                exec {
-                  commandLine = ['npm', 'install']
-                  runIf = 'passed'
-                  workingDir = 'codesigning'
-                }
-                exec {
-                  commandLine = ['node', 'lib/bump_helm_chart_version.js']
-                  runIf = 'passed'
-                  workingDir = 'codesigning'
-                }
-              }
-            }
             job('update_at_gocd_chart_repo') {
               elasticProfileId = 'ecs-gocd-dev-build'
               environmentVariables = [
